5be30a76a1ed2fcb2b02e94f4bc60099bc491820
OK_Kehan <tang.keh@northeastern.edu>
2023-08-30 17:03:46 -0700

    finish the robotfriend project

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users =>this.setState({robots: users}));
+    }
+
+    // use => to refer the upper level this function
+    onSearchChange = (event) => {
+        this.setState( {searchfield: event.target.value})
+    }
+
+    render(){
+        const {robots, searchfield} = this.state;
+        const fillteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+
+        if (robots.length === 0){
+            return <h1>Loading</h1>
+        }else{
+            return (
+                <div className = 'tc'>
+                    <h1 className = 'f1'>RobotFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = {fillteredRobots}/>
+                    </Scroll>
+                   
+                </div>  
+                
+            );
+        }
+    }
+
+}
+
+export default App;
